efd14515d91ab9388ca36a76eeb54efab0047b79
deepakchoudhary <deepak76781999@gmail.com>
2022-02-01 01:26:30 +0530

    main

--- contectform.php
+++ contectform.php
@@ -1,96 +1,97 @@
-<form action="./sendmail.php" method="post" name="contactform" id="contactform" class="animated out" data-animation="fadeInUp" data-delay="0">
 
-                  <fieldset>
-                    <div class="form-group">
-                      <input class="form-control br-b" type="text" name=" name" id="name" placeholder="Name">
-                    </div>
-                    
-                    <div class="form-group">
-                      <!-- <label for="country">Country</label>
-                    <select id="country" name="country">
-                      <option value="australia">Australia</option>
-                      <option value="canada">Canada</option>
-                      <option value="usa">USA</option>
-                      <option value="India">India</option>
-                    </select> -->
-                  
-                      </div>
+<?php
+include_once('mail.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-                    <div class="form-group">
-                      <input class="form-control br-b" type="email" name="email" id="email" placeholder="Email">
-                    </div>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TrickuWeb</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles/website.css">
+</head>
 
-                    <div class="form-group">
-                      <textarea class="form-control br-b" name="message" id="message" placeholder="Message..."></textarea>
-                    </div>
-                  </fieldset>
-                  
-                  <!-- submit button -->
-                  <div class="form-group">
-                    <input type="submit" name="submit" value="Send message" id="submit" class="btn btn-sm btn-dark">
-                  </div>
+<body>
 
-                  <div id="alert"></div>
 
-                </form>
-                <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  <div class="container mt-5 mycareer">
+    <div class="row">
+      <div class="col-md-8" style="margin:0 auto; float:none;">
+        <h3 align="center">Build Your Career With TrickuWeb</h3>
+        <br />
+        <h4 align="center">Please fill this form</h4><br />
+                <form method="post" enctype="multipart/form-data">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Enter Name</label>
+                <input type="text" name="name" placeholder="Enter Name" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label>Enter Address</label>
+                <textarea name="address" placeholder="Enter Address" class="form-control" required></textarea>
+              </div>
+              <div class="form-group">
+                <label>Enter Email Address</label>
+                <input type="email" name="email" class="form-control" placeholder="Enter Email Address" required />
+              </div>
+              <div class="form-group">
+                <label>Select Your Skills</label>
+                <select name="skills[]" class="form-control" multiple  style="height:150px;" required>
+                  <option value="Flutter">Flutter</option>
+                  <option value="Laravel">Laravel</option>
+                  <option value="Django">Django</option>
+                  <option value="Firebase">Firebase</option>
+                  <option value="Android">Android</option>
+                  <option value="Data Engineer">Data Engineer</option>
+                  <option value="AI & Data Science">AI & Data Science</option>
+                  <option value="Arduino & Raspberry Pi">Arduino & Raspberry Pi</option>
+                </select>
+              </div>
 
-require ('mailer/Exception.php');
-require ('mailer/SMTP.php');
-require ('mailer/PHPMailer.php');
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Select Year of Experience</label>
+                <select name="experience" class="form-control" required>
+                  <option value="">Select Experience</option>
+                  <option value="0-1 years">0-1 years</option>
+                  <option value="2-3 years">2-3 years</option>
+                  <option value="4-5 years">4-5 years</option>
+                  <option value="6-7 years">6-7 years</option>
+                  <option value="8-9 years">8-9 years</option>
+                  <option value="10 or more years">10 or more years</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label>Enter Mobile Number</label>
+                <input type="text" name="mobile" placeholder="Enter Mobile Number" class="form-control" pattern="\d*" required />
+              </div>
+              <div class="form-group">
+                <label>Select Your Resume</label>
+                <input type="file" name="resume" accept=".doc,.docx,.pdf"/ required>
+              </div>
+              <div class="form-group">
+                <label>Enter Additional Information</label>
+                <textarea name="additional_information" placeholder="Enter Additional Information" class="form-control" required rows="8"></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="form-group" align="center">
+            <input type="submit" name="submit" value="Submit" class="btn btn-lg btn-success" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 
-  if(isset($_POST['submit'])){
-      $email = $_post['email'];
-    $name = $_post['name'];
-    $msg = $_POST['message'];
-     
+</body>
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-try{
+</html>
 
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'deepak.livechek1999@gmail.com';                     //SMTP username
-    $mail->Password   = 'Deepak@2004';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('deepak.livechek1999@gmail.com','codeland');
-    $mail->addAddress('deepak.livechek1999@gmail.com');     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
-    // $mail->addReplyTo('deepak.livechek1999@gmail.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'contect us';
-    $mail->Body    = "name: $name <br> email: $email<br> massege:$msg";
-    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    
-       $mail->send();
-       echo "<script> alert(' Your Message has been sent successfully')</script>";
-       
-        
-       } catch(Exception $e) {
-        echo " <script> alert('Mailer Error:  {$mail->ErrorInfo}')</script>";
-        
-    }
-}
-
- ?>
